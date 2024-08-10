4240ffa08d4b690c72168a3574f059f8f0c92794
icaroQre <icaro.queiroz.reccanello@gmail.com>
2024-08-10 18:09:00 -0300

    feat: update badge rating BarberShopItem component

--- src/_components/barbershop-item.tsx
+++ src/_components/barbershop-item.tsx
@@ -2,6 +2,8 @@ import { Barbershop } from "@prisma/client"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { StarIcon } from "lucide-react"
 
 interface barbershopItemProps {
   barbershop: Barbershop
@@ -18,6 +20,11 @@ const BarbershopItem = ({ barbershop }: barbershopItemProps) => {
             fill
             className="rounded-2xl object-cover"
           />
+
+          <Badge variant={"secondary"} className="absolute left-2 top-2 gap-1">
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold"> 5,0 </p>
+          </Badge>
         </div>
 
         <div className="flex flex-col px-2 py-3">
