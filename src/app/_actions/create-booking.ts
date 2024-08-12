5ceacc9f3a0b2f83f5c3b0df6bdaaf04e3222346
icaroQre <icaro.queiroz.reccanello@gmail.com>
2024-08-12 15:56:31 -0300

    feat: ensure only authenticate user can book

--- src/app/_actions/create-booking.ts
+++ src/app/_actions/create-booking.ts
@@ -1,15 +1,24 @@
 "use server"
 
+import { authOptions } from "@/_lib/auth"
 import { db } from "@/_lib/prisma"
+import { getServerSession } from "next-auth"
+import { revalidatePath } from "next/cache"
 
 interface createBookingParams {
   serviceId: string
-  userId: string
   date: Date
 }
 
 export const createBooking = async (params: createBookingParams) => {
+  const user = await getServerSession(authOptions)
+
+  if (!user) {
+    throw new Error("Usuário não autenticado!")
+  }
+
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: (user as any).user.id },
   })
+  revalidatePath("/barbershops/[id]")
 }
